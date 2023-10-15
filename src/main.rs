75f91e209100d3c95b6429e72f89e0311ae6e531
defniclt <defnic@defnic.dn>
2023-10-15 19:12:53 +0200

    rodio implemented

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
 use macroquad::{input, miniquad::conf, prelude::*};
+use rodio::{self, buffer};
 
 fn conf() -> conf::Conf {
     conf::Conf {
         window_title: "audio to image".to_owned(),
-        window_width: 800,
-        window_height: 600,
+        window_width: 1280,
+        window_height: 720,
         ..Default::default()
     }
 }
 
 #[macroquad::main(conf)]
 async fn main() {
-    let texture = load_texture("images/test_img.png").await.unwrap();
+    let texture: Texture2D = load_texture("images/test_img.png").await.unwrap();
+    let mut tex_params: DrawTextureParams = DrawTextureParams::default();
+
+    let test_img = load_image("images/test_img.png").await.unwrap();
+    let texture_test = Texture2D::from_image(&test_img);
+    let test_img_data = test_img.get_image_data();
+    let b_data = calculate_brightness(test_img_data);
+
+    println!("b_data: {:?}", b_data);
+
+    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+    let asd = buffer::SamplesBuffer::new(2, 44100, b_data);
+
+    // stream_handle.play_raw(asd.convert_samples()).unwrap();
 
     let mut playhead_pos = 0.0;
-    let playback_speed = 100.0; // Fix speed to scale with image width!
+    let playback_speed = 10.0; // Fix speed to scale with image width!
 
     let mut is_playing = false;
 
@@ -24,9 +38,14 @@ async fn main() {
         let window_height = screen_height();
         let window_width = screen_width();
 
+        tex_params.dest_size = Some(Vec2 {
+            x: window_width,
+            y: window_height,
+        });
+
         if is_playing {
             if playhead_pos < window_width {
-                playhead_pos += delta * playback_speed;
+                playhead_pos += window_width * 0.01 * playback_speed * delta;
             } else {
                 playhead_pos = 0.0;
             }
@@ -38,32 +57,17 @@ async fn main() {
 
         clear_background(GRAY);
 
-        // draw_texture_ex(
-        //     &texture,
-        //     0.0,
-        //     0.0,
-        //     WHITE,
-        //     DrawTextureParams {
-        //         dest_size: Some(Vec2::new(window_width, window_height)),
-        //         ..Default::default()
-        //     },
-        // );
+        draw_texture_ex(&texture_test, 0.0, 0.0, WHITE, tex_params.to_owned());
 
-        draw_rectangle(
-            20.0,
-            20.0,
-            window_height - 40.0,
-            window_height - 40.0,
-            GREEN,
-        );
-
-        // Draw line from top to bottom
+        // Playhead
         draw_line(playhead_pos, 0.0, playhead_pos, window_height, 4.0, RED);
 
+        // Debug info stuff
         debug_info(window_width, window_height, playhead_pos);
 
         next_frame().await;
 
+        // Exit the app
         if is_key_pressed(KeyCode::Escape) {
             break;
         }
@@ -83,4 +87,22 @@ fn debug_info(window_width: f32, window_height: f32, playhead_pos: f32) {
 
     let pp = format!("playhead pos: {}", playhead_pos);
     draw_text(&pp, 20.0, 60.0, 24.0, RED);
+}
+
+fn calculate_brightness(data: &[[u8; 4]]) -> Vec<f32> {
+    let mut brightness_data = Vec::new();
+
+    for pixel in data {
+        let r = pixel[0] as f32;
+        let g = pixel[1] as f32;
+        let b = pixel[2] as f32;
+
+        // Calculate average brightness (ignoring alpha)
+        // Scale to [0.0, 1.0]
+
+        // Convert to u8 and append to the result
+        brightness_data.push((r + g + b) / 3.0 / 255.0);
+    }
+
+    brightness_data
 }
